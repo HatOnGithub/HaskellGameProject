3e1a3b4d565e03df2988189bd69e260d753ba033
Hat (Denny) <j.t.d.chen@students.uu.nl>
2023-10-25 22:59:18 +0200

    shit fits now

--- src/QuadTree.hs
+++ src/QuadTree.hs
@@ -5,7 +5,7 @@ import Model
 -- QuadTree related stuff
 
 thresholdObjectsPerQuadrant :: Int
-thresholdObjectsPerQuadrant = 2
+thresholdObjectsPerQuadrant = 6
 
 buildQuadTree :: (CollisionObject a, Eq a) => [a] -> Model.Point -> QuadTree a
 buildQuadTree l  worldSize = insertAll l (Node ((0,0),  worldSize) [] EmptyLeaf EmptyLeaf EmptyLeaf EmptyLeaf)
@@ -16,14 +16,15 @@ insertAll (x:xs) t@(Node bb objs tl tr bl br)   = insertAll xs (insert x t)
 
 
 insert :: (CollisionObject a, Eq a) => a -> QuadTree a -> QuadTree a
-insert x t@(Node bb xs tl tr bl br) | not (x `fitsIn` bb) = t
-                                    | hasSubNodes t && any (\q -> x `fitsIn` getBB q) [tl, tr, bl, br] 
-                                        = insertIntoQuadrant x t
-                                    | length xs < thresholdObjectsPerQuadrant ||         -- if it fits and there is room OR
-                                      all (\q -> not (x `fitsIn` getBB q)) quadrants     -- the max is reached and it doesn't fit into any subnodes
-                                        = Node bb (x : xs) tl tr bl br                   -- add to layer
-                                    | any (\q -> x `fitsIn` getBB q) quadrants 
-                                        = insertAll (x:xs) (Node bb [] ntl ntr nbl nbr)  -- if the max is reached, attempt subinsert of everything in that layer and move on
+insert x t@(Node bb xs tl tr bl br) 
+    | not (x `fitsIn` bb) = t
+    | hasSubNodes t && any (\q -> x `fitsIn` getBB q) [tl, tr, bl, br] 
+        = insertIntoQuadrant x t
+    | length xs < thresholdObjectsPerQuadrant ||         -- if it fits and there is room OR
+      all (\q -> not (x `fitsIn` getBB q)) quadrants     -- the max is reached and it doesn't fit into any subnodes
+        = Node bb (x : xs) tl tr bl br                   -- add to layer
+    | any (\q -> x `fitsIn` getBB q) quadrants 
+        = insertAll (x:xs) (Node bb [] ntl ntr nbl nbr)  -- if the max is reached, attempt subinsert of everything in that layer and move on
     where quadrants@[ntl, ntr, nbl, nbr] = map newNode (toQuadrants bb)
 
 newNode :: CollisionObject a => BoundingBox -> QuadTree a
@@ -36,10 +37,11 @@ getBB :: CollisionObject a => QuadTree a -> BoundingBox
 getBB (Node bb _ _ _ _ _) = bb
 
 insertIntoQuadrant :: (CollisionObject a, Eq a) => a -> QuadTree a -> QuadTree a
-insertIntoQuadrant x t@(Node bb objs tl tr bl br) | x `fitsIn` bbOf tl = Node bb objs (insert x tl) tr bl br
-                                                  | x `fitsIn` bbOf tr = Node bb objs tl (insert x tr) bl br
-                                                  | x `fitsIn` bbOf bl = Node bb objs tl tr (insert x bl) br
-                                                  | x `fitsIn` bbOf br = Node bb objs tl tr bl (insert x br)
+insertIntoQuadrant x t@(Node bb objs tl tr bl br) 
+    | x `fitsIn` bbOf tl = Node bb objs (insert x tl) tr bl br
+    | x `fitsIn` bbOf tr = Node bb objs tl (insert x tr) bl br
+    | x `fitsIn` bbOf bl = Node bb objs tl tr (insert x bl) br
+    | x `fitsIn` bbOf br = Node bb objs tl tr bl (insert x br)
     where bbOf (Node bb _ _ _ _ _) = bb
 
 toQuadrants :: BoundingBox -> [BoundingBox]
@@ -48,12 +50,13 @@ toQuadrants (tl, (sx, sy)) = [(tl, half), (tl `addX` hx, half), (tl `addY` hy, h
 
 getPossibleCollisionPartners :: (CollisionObject a,  CollisionObject b) => a -> QuadTree b -> [b]
 getPossibleCollisionPartners _ EmptyLeaf = []
-getPossibleCollisionPartners obj n@(Node bb objs tl tr bl br )  | length objs  >= thresholdObjectsPerQuadrant = collapse n
-                                                                | obj `fitsIn` bb   = concat [ getPossibleCollisionPartners obj tl
-                                                                                             , getPossibleCollisionPartners obj tr
-                                                                                             , getPossibleCollisionPartners obj bl
-                                                                                             , getPossibleCollisionPartners obj br ]
-                                                                | otherwise         = []
+getPossibleCollisionPartners obj n@(Node bb objs tl tr bl br )
+    | length objs  >= thresholdObjectsPerQuadrant = collapse n
+    | obj `fitsIn` bb   = concat    [ getPossibleCollisionPartners obj tl
+                                    , getPossibleCollisionPartners obj tr
+                                    , getPossibleCollisionPartners obj bl
+                                    , getPossibleCollisionPartners obj br ]
+    | otherwise         = []
 
 collapse :: CollisionObject a => QuadTree a -> [a]
 collapse EmptyLeaf = []
